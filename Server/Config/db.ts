2e36f89391973bdf7b935cc3073e98f3ecb66625
Ibrahim <iozturk987@gmail.com>
2022-06-26 01:05:09 -0400

    fixed problem with heroku deployment

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://127.0.0.1/books229";
-export const RemoteURI = "your remote host information goes here"
+export const RemoteURI = "mongodb+srv://ibrahim:JqmkYSPLsLPjwrXO@cluster0.aefmpx9.mongodb.net/?retryWrites=true&w=majority"
 export const HostName = "LocalHost";
 export const Secret = "someSecret";
